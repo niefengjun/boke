da65d81013fcf317e66178e5927c5a8838edf965
结婚的傻熊 <yanglinnie@126.com>
2016-02-27 17:57:55 +0800

    update

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,14 @@ var express = require('express');
 var router = express.Router();
 var pageLogin = require('../lib/pageLogin.js');
 var pageBlog = require('../lib/pageBlog.js');
+var pageIndex = require('../lib/pageIndex.js');
 /* GET home page. */
-router.get('/', pageLogin.index);
+
+router.get('/',pageIndex.index) ;
+//router.get('/search',pageSeach.index) ;
+// app.get('/sogousiteverification.txt',pageblogindex.baidu) ;
+router.get('/page_:page.html',pageIndex.index) ;
+
 router.get('/admin', pageLogin.index);
 router.get('/Login/', pageLogin.login);
 router.all('*', pageLogin.checkLogin);
