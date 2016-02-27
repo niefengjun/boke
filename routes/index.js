c1619933afe0fba74c81014f75ba27de1cb86ac4
结婚的傻熊 <yanglinnie@126.com>
2016-02-27 16:15:34 +0800

    update

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,7 @@ var pageBlog = require('../lib/pageBlog.js');
 router.get('/', pageLogin.index);
 router.get('/admin', pageLogin.index);
 router.get('/Login/', pageLogin.login);
+router.all('*', pageLogin.checkLogin);
 router.all('/admin/blog/',pageBlog.index) ;
 
 module.exports = router;
