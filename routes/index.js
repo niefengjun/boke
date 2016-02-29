7d78a8c6d2fbe89ccb0fe877450a5184e1da579e
结婚的傻熊 <yanglinnie@126.com>
2016-02-29 11:02:26 +0800

    update

--- routes/index.js
+++ routes/index.js
@@ -6,19 +6,14 @@ var pageIndex = require('../lib/pageIndex.js');
 var pagecategory = require('../lib/pagecategory.js');
 var pageSeach = require('../lib/pageSeach.js');
 /* GET home page. */
-
 router.get('/',pageIndex.index) ;
 router.get('/search',pageSeach.index) ;
-// app.get('/sogousiteverification.txt',pageblogindex.baidu) ;
-
 router.get('/blogyuedu/:id/',pageIndex.findOneid_yuedu) ;
 router.get('/page_:page.html', pageIndex.index);
 router.get('/category/:id.html', pagecategory.index);
 router.get('/category/:id/:page.html', pagecategory.index);
-
 router.get('/admin', pageLogin.index);
 router.get('/Login/', pageLogin.login);
 router.all('*', pageLogin.checkLogin);
 router.all('/admin/blog/',pageBlog.index) ;
-
 module.exports = router;
