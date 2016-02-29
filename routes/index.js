6d14bb5f0d13e3986241122f307262b8d2f37541
结婚的傻熊 <yanglinnie@126.com>
2016-02-29 09:51:16 +0800

    update

--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,15 @@ var router = express.Router();
 var pageLogin = require('../lib/pageLogin.js');
 var pageBlog = require('../lib/pageBlog.js');
 var pageIndex = require('../lib/pageIndex.js');
+var pagecategory = require('../lib/pagecategory.js');
 /* GET home page. */
 
 router.get('/',pageIndex.index) ;
 //router.get('/search',pageSeach.index) ;
 // app.get('/sogousiteverification.txt',pageblogindex.baidu) ;
-//router.get('/page_:page.html',pageIndex.index) ;
+router.get('/page_:page.html', pageIndex.index);
+router.get('/category/:id.html', pagecategory.index);
+router.get('/category/:id/:page.html', pagecategory.index);
 
 router.get('/admin', pageLogin.index);
 router.get('/Login/', pageLogin.login);
