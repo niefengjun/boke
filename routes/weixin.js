d15a3624cac43bed208e72beda8065921e2b1bdd
结婚的傻熊 <yanglinnie@126.com>
2016-03-05 16:28:40 +0800

    update

--- routes/weixin.js
+++ routes/weixin.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 var pageWexin=require('../lib/pageWeixin.js') ;
 /* GET home page. */
-router.get('/:id/',pageWexin.index) ;
+router.all('/:id/',pageWexin.index) ;
 
 router.all('/getauthorizeurl/:id/', pageWexin.getAuthorizeURL); //生成微信访问连接
 router.all('/getaccesstoken/:id/', pageWexin.getAccessToken); //根据code 换取用户信息
