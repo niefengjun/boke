2933a496424b597279b33cf3f9984d7110770160
结婚的傻熊 <yanglinnie@126.com>
2016-04-14 09:39:09 +0800

    update

--- app.js
+++ app.js
@@ -30,6 +30,8 @@ app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public')));
 app.set('views', path.join(__dirname, 'views'));
 app.use(bodyParser.json({ "limit":"80000kb"}));
+app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({limit: '50mb'}));
 app.engine('.html', ejs.__express);
 app.set('view engine', 'html');
 app.use('/weixin/',weixin);
