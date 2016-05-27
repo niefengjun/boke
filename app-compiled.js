e1ed20e69fd134ef5e5e89dd64d24df64ac65631
结婚的傻熊 <yanglinnie@126.com>
2016-05-27 09:25:06 +0800

    update

--- app-compiled.js
+++ app-compiled.js
@@ -15,7 +15,6 @@ var session = require('express-session');
 var RedisStore = require('connect-redis')(session);
 var config = require('./config/config.js');
 
-
 var app = express();
 
 // view engine setup
@@ -28,36 +27,35 @@ app.use(cookieParser());
 app.use(require('stylus').middleware(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({
-    extended: true,limit: '50mb'
-}))
+    extended: true, limit: '50mb'
+}));
 //app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public')));
 app.set('views', path.join(__dirname, 'views'));
-app.use(bodyParser.json({ "limit":"80mb"}));
+app.use(bodyParser.json({ "limit": "80mb" }));
 
 app.use(cookieParser('keyboard cat'));
 
 app.use(session({
     store: new RedisStore({
-        host:config.redisdb.redis_host,
+        host: config.redisdb.redis_host,
         port: config.redisdb.redis_port,
-        pass:config.redisdb.redis_psd,
-        db:0
+        pass: config.redisdb.redis_psd,
+        db: 0
     }),
-    resave:false,
-    saveUninitialized:false,
+    resave: false,
+    saveUninitialized: false,
     secret: 'keyboard cat',
-    cookie: { maxAge: 60*60*24*1000}
-}))
+    cookie: { maxAge: 60 * 60 * 24 * 1000 }
+}));
 //app.use(express.json({limit: '50mb'}));
 //app.use(express.urlencoded({limit: '50mb'}));
 app.engine('.html', ejs.__express);
 app.set('view engine', 'html');
-app.use('/weixin/',weixin);
+app.use('/weixin/', weixin);
 app.use('/test', test);
-app.use('/manager',admin);
+app.use('/manager', admin);
 app.use('/', routes);
-
 
 app.use("/ueditor/ue/", ueditor(path.join(__dirname, 'public'), function (req, res, next) {
     // ueditor 客户发起上传图片请求
@@ -72,15 +70,13 @@ app.use("/ueditor/ue/", ueditor(path.join(__dirname, 'public'), function (req, r
     }
     //  客户端发起图片列表请求
     else if (req.query.action === 'listimage') {
-        var dir_url = '/images/ueditor/';
-        res.ue_list(dir_url); // 客户端会列出 dir_url 目录下的所有图片
-    }
-
-    else {
-        // console.log('config.json')
-        res.setHeader('Content-Type', 'application/json');
-        res.redirect('/ueditor/nodejs/config.json');
-    }
+            var dir_url = '/images/ueditor/';
+            res.ue_list(dir_url); // 客户端会列出 dir_url 目录下的所有图片
+        } else {
+                // console.log('config.json')
+                res.setHeader('Content-Type', 'application/json');
+                res.redirect('/ueditor/nodejs/config.json');
+            }
 }));
 
 // catch 404 and forward to error handler
@@ -114,7 +110,7 @@ app.use("/ueditor/ue/", ueditor(path.join(__dirname, 'public'), function (req, r
 //  });
 //});
 
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
     res.render('404');
 });
 
@@ -122,14 +118,14 @@ app.use(function(req, res, next) {
 
  */
 
-
 var log4js = require('log4js');
 log4js.configure('config/log4js.json');
 global.logger = log4js.getLogger();
-app.use(log4js.connectLogger(logger,
-    {
-        format: ':method :url HTTP/:http-version :status :res[content-length] - :response-time ms',
-        nolog: ["\\.jpg", "\\.ico", "\\.png", "\\.gif", "\\.js", "\\.css", "\\.swf"]
-    }));
+app.use(log4js.connectLogger(logger, {
+    format: ':method :url HTTP/:http-version :status :res[content-length] - :response-time ms',
+    nolog: ["\\.jpg", "\\.ico", "\\.png", "\\.gif", "\\.js", "\\.css", "\\.swf"]
+}));
 
 module.exports = app;
+
+//# sourceMappingURL=app-compiled.js.map
---
+++ config/config-compiled.js
@@ -0,0 +1,10 @@
+exports.redisdb = {
+    //redis_host: '172.28.19.72',
+    redis_host: '120.24.196.66',
+    redis_port: 6379,
+    redis_psd: 'Nie@0428',
+    redis_db: 0,
+    expire: 600 //redis过期时间
+};
+
+//# sourceMappingURL=config-compiled.js.map
