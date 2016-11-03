6c17053b46cf58033ba4741d4c40d3215bfa74e2
结婚的傻熊 <yanglinnie@126.com>
2016-11-03 17:25:49 +0800

    update

--- test.js
+++
@@ -1,9 +0,0 @@
-var blogword=require('./lib/nedb/blogword')
-blogword.list().then(function(data)
-    {
-        console.log(data) ;
-    }
-)
-client.getUser('openid', function (err, result) {
-    var userInfo = result;
-});
